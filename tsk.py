2f44d0b191e9a6019d53ffef8a7795f883c0a8b0
Arthur Sabioni <arthur_vrb@hotmail.com>
2023-03-16 08:35:57 -0300

    Every epoch tests every point, and graphs plotted

--- tsk.py
+++ tsk.py
@@ -1,29 +1,60 @@
 
 import numpy as np
-import decimal
+import random
 import math
 
+import matplotlib.pyplot as plt
 
 def function(x):
     return x*x
 
+
+def plot_2d(results: np.array, expected: np.array):
+
+    plt.figure()
+
+    plt.plot(results.T[0], results.T[1])
+    plt.plot(expected.T[0], expected.T[1])
+
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
+
+
 class FuzzyTsk:
     
-    def generate_new_points(self):
-        self.points = np.array([(x, self.f(x)) for x in np.random.uniform(-2, 2, self.n_points)])
+    def shuffle_points(self):
+        self.points_shuffled = np.random.permutation(self.points)
     
-    def __init__(self, function, n_points=1000, epocas=1000, min_x=-2, max_x=2, alpha=0.01):
+    def __init__(self, function, step=0.01, epocas=1000, min_x=-2, max_x=2, alpha=0.01):
         
         self.f = function
-        self.n_points = n_points
+        self.step = step
         self.epocas = epocas
         self.min_x = min_x
         self.max_x = max_x
         self.alpha = alpha
         
         # gerar pontos
-        self.points = []
-        self.generate_new_points()
+        self.points = np.array([(x, self.f(x)) for x in np.arange(-2,2,self.step)])
+        self.shuffle_points()
+        
+    def extimate_y(self, x1m, x1s, p1, q1, x2m, x2s, p2, q2, x, return_all=True):
+        #Calcular w1 e w2 para calcular y de cada um
+        w1 = math.exp(-1/2*math.pow((x-x1m)/x1s, 2))
+        w2 = math.exp(-1/2*math.pow((x-x2m)/x2s, 2))
+        
+        w1n = w1/(w1 + w2)
+        w2n = w2/(w1 + w2)
+        
+        y1 = p1*x + q1
+        y2 = p2*x + q2
+        
+        y = w1n*y1 + w2n*y2
+        
+        if return_all:
+            return w1, w2, w1n, w2n, y1, y2, y
+        return y
         
     def execute(self):
         
@@ -42,17 +73,8 @@ class FuzzyTsk:
         for epoca in range(0, self.epocas):
             # fazer inferencia de todos os pontos gerados
             for i,x in enumerate(self.points.T[0]):
-                #Calcular w1 e w2 para calcular y de cada um
-                w1 = math.exp(-1/2*math.pow((x-x1m)/x1s, 2))
-                w2 = math.exp(-1/2*math.pow((x-x2m)/x2s, 2))
                 
-                w1n = w1/(w1 + w2)
-                w2n = w2/(w1 + w2)
-                
-                y1 = p1*x + q1
-                y2 = p2*x + q2
-                
-                y = w1n*y1 + w2n*y2
+                w1, w2, w1n, w2n, y1, y2, y = self.extimate_y(x1m, x1s, p1, q1, x2m, x2s, p2, q2, x)
                 yd = self.points[i][1]
                 e = y - yd
                 
@@ -67,10 +89,16 @@ class FuzzyTsk:
                 x2m = x2m - self.alpha*e*w1*(y1-y2)/(pow(w1+w2, 2))*x-x2m/pow(x1s,2)*math.exp(math.pow(-1/2*((x))/x2s,2))
             
                 somaErros = somaErros + e
-            # gerar novos pontos para a próxima iteração
-            self.generate_new_points()
-            
+                
             # plotar a curva de tempos em tempos
+            extimated_points = np.array([(x, self.extimate_y(x1m, x1s, p1, 
+                                                             q1, x2m, x2s, 
+                                                             p2, q2, x, return_all=False)) for x in np.arange(-2,2,self.step)])
+            plot_2d(extimated_points, self.points)
+            
+            # gerar novos pontos para a próxima iteração
+            self.shuffle_points()
+            
             print(str(epoca) + ": " + str(somaErros))
         
 if __name__ == "__main__":
