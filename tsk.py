08d794afa96ff2c99fae63e1ea6dacf709897bdc
Arthur Sabioni <arthur_vrb@hotmail.com>
2023-03-16 10:03:51 -0300

    Algoryth mostly working

--- tsk.py
+++ tsk.py
@@ -9,16 +9,18 @@ def function(x):
     return x*x
 
 
-def plot_2d(results: np.array, expected: np.array):
+def plot_2d(results: np.array, expected = [], labels=[''], title='', block=False):
 
     plt.figure()
 
-    plt.plot(results.T[0], results.T[1])
-    plt.plot(expected.T[0], expected.T[1])
+    plt.plot(results.T[0], results.T[1], '-b', label=labels[0])
+    if len(expected):
+        plt.plot(expected.T[0], expected.T[1], '-r', label=labels[1])
 
+    plt.title(title)
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.show()
+    plt.show(block=block)
 
 
 class FuzzyTsk:
@@ -58,7 +60,6 @@ class FuzzyTsk:
         
     def execute(self):
         
-        # iniciar p1, p2 e q
         x1m = -2
         x1s = 1
         p1 = -2
@@ -67,10 +68,20 @@ class FuzzyTsk:
         x2m = 2
         x2s = 1
         p2 = 2
-        q2 =0
-        
+        q2 = 0
+                
         somaErros = 0
+        listErros = []
         for epoca in range(0, self.epocas):
+                
+            # plotar a curva de tempos em tempos
+            if not epoca or epoca % (self.epocas//10) == 0 or epoca == self.epocas - 1:
+                extimated_points = np.array([(x, self.extimate_y(x1m, x1s, p1, 
+                                                                q1, x2m, x2s, 
+                                                                p2, q2, x, return_all=False)) for x in np.arange(-2,2,self.step)])
+                plot_2d(extimated_points, self.points, ['estimado', 'esperado'],
+                        'Após {} épocas - Erro = {}'.format(epoca, round(somaErros,2)))
+                
             # fazer inferencia de todos os pontos gerados
             for i,x in enumerate(self.points.T[0]):
                 
@@ -85,23 +96,23 @@ class FuzzyTsk:
                 q1 = q1 - self.alpha*e*w1n
                 q2 = q2 - self.alpha*e*w2n
                 
-                x1m = x1m - self.alpha*e*w2*(y1-y2)/(pow(w1+w2, 2))*x-x1m/pow(x1s,2)*math.exp(math.pow(-1/2*((x))/x1s,2))
-                x2m = x2m - self.alpha*e*w1*(y1-y2)/(pow(w1+w2, 2))*x-x2m/pow(x1s,2)*math.exp(math.pow(-1/2*((x))/x2s,2))
+                x1m = x1m - self.alpha*e*w2*(y1-y2)/(pow(w1+w2, 2))*x-x1m/pow(x1s,2)*math.exp(-1/2*math.pow((x-x1m)/x1s,2))
+                x2m = x2m - self.alpha*e*w1*(y2-y1)/(pow(w1+w2, 2))*x-x2m/pow(x2s,2)*math.exp(-1/2*math.pow((x-x2m)/x2s,2))
+                
+                x1s = x1s - self.alpha*e*w2*(y1-y2)/(pow(w1+w2, 2))*pow(x-x1m,2)/pow(x1s,3)*math.exp(-1/2*math.pow((x-x1m)/x1s,2))
+                x2s = x2s - self.alpha*e*w1*(y2-y1)/(pow(w1+w2, 2))*pow(x-x2m,2)/pow(x2s,3)*math.exp(-1/2*math.pow((x-x2m)/x2s,2))
             
                 somaErros = somaErros + e
-                
-            # plotar a curva de tempos em tempos
-            extimated_points = np.array([(x, self.extimate_y(x1m, x1s, p1, 
-                                                             q1, x2m, x2s, 
-                                                             p2, q2, x, return_all=False)) for x in np.arange(-2,2,self.step)])
-            plot_2d(extimated_points, self.points)
             
             # gerar novos pontos para a próxima iteração
             self.shuffle_points()
             
-            print(str(epoca) + ": " + str(somaErros))
+            listErros.append((int(epoca), somaErros))
+            #print(str(epoca) + ": " + str(somaErros))
+            
+        plot_2d(np.array(listErros), labels=['erro'], title='Erro por época', block=True)
         
 if __name__ == "__main__":
     
-    ft = FuzzyTsk(function, alpha=0.001)
+    ft = FuzzyTsk(function, alpha=0.001, epocas=1000)
     ft.execute()
